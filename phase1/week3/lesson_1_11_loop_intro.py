5751446578982ef7f85bb16bd51e135191992616
howardliao1 <howardliao@ucarer.com.tw>
2026-07-16 13:38:46 +0800

    完成 Lesson 1-11 迴圈概念練習

--- phase1/week3/lesson_1_11_loop_intro.py
+++ phase1/week3/lesson_1_11_loop_intro.py
@@ -1,6 +1,8 @@
+# A loop would reduce this repeated code
 print("Send notification.")
 print("Send notification.")
 print("Send notification.")
 print("Send notification.")
 print("Send notification.")
+
 print("All notifications sent.")
