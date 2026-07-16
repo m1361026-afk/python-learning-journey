acd0da8ba9355d330ee26fdbc401af38f9d4cd76
howardliao1 <howardliao@ucarer.com.tw>
2026-07-16 14:48:53 +0800

    完成 Lesson 1-12 for 迴圈與 range 練習

--- phase1/week3/lesson_1_12_dynamic_odd_numbers.py
+++ phase1/week3/lesson_1_12_dynamic_odd_numbers.py
@@ -2,7 +2,7 @@
 max_number = int(input("Please enter a maximum number: "))
 
 if 1 <= max_number <= 100:
-    # Make sure the max_number can be included
+    # Include the maximum value when it is odd
     stop = max_number + 1
     # Print all odd numbers from 1 to the maximum number
     for i in range(1, stop, 2):
